b63959347a5c58703ab9ca285625600597ee0581
Michael Lewandowski <milewgit@gmail.com>
2014-03-23 15:36:43 -0400

    Extract methods initialize_page() and create_css_settings_control().

--- script.js
+++ script.js
@@ -2,50 +2,59 @@
   
   // TODO: not yet refactored
   
-  $('div[data-type="range"]').each( function() {
-
-    var element = this.dataset.element;
-    var attribute = this.dataset.attribute;
-    
-    var min = this.dataset.min || -4;
-    var max = this.dataset.max || 4;
-    var defaultValue = this.dataset.defaultValue || 0;
-    var presetValue = this.dataset.presetValue || 0;
-    
-    var attributeLabel = $("<label>" + (this.dataset.attributeLabel || attribute) + ":</label>");
-    var input = $("<input type='text' readonly />");
-    var unitsLabel = $("<label>em;</label>");
-    var range = $("<input type='range' min='" + min + "' max='" + max + "' />");
-    
-    $(this).append(attributeLabel).append(input).append(unitsLabel).append(range);
-    
-    range.on('input change', function() {
-      f(this.value);
-    });
-    
-    $('#preset-button').on('click', function() {
-      f(presetValue);
-    });
-    
-    $('#reset-button').on('click', function() {
-      f(defaultValue);
-    });
-    
-    var that = $(this);
-    var f = function(value) {
-      input.val(value);
-      range.val(value);
-      $('section[id="example"] ' + element).css(attribute, value + 'em');
-      
-      if (value == defaultValue) {
-          that.removeClass("non-default-value");
-      } else {
-          that.addClass("non-default-value");
-      }
-    }
-
-  });
+  function create_css_settings_controls() {
   
-  $('#reset-button').click();
+    $('div[data-type="range"]').each( function() {
+
+      var element = this.dataset.element;
+      var attribute = this.dataset.attribute;
+    
+      var min = this.dataset.min || -4;
+      var max = this.dataset.max || 4;
+      var defaultValue = this.dataset.defaultValue || 0;
+      var presetValue = this.dataset.presetValue || 0;
+    
+      var attributeLabel = $("<label>" + (this.dataset.attributeLabel || attribute) + ":</label>");
+      var input = $("<input type='text' readonly />");
+      var unitsLabel = $("<label>em;</label>");
+      var range = $("<input type='range' min='" + min + "' max='" + max + "' />");
+    
+      $(this).append(attributeLabel).append(input).append(unitsLabel).append(range);
+    
+      range.on('input change', function() {
+        f(this.value);
+      });
+    
+      $('#preset-button').on('click', function() {
+        f(presetValue);
+      });
+    
+      $('#reset-button').on('click', function() {
+        f(defaultValue);
+      });
+    
+      var that = $(this);
+      var f = function(value) {
+        input.val(value);
+        range.val(value);
+        $('section[id="example"] ' + element).css(attribute, value + 'em');
+      
+        if (value == defaultValue) {
+            that.removeClass("non-default-value");
+        } else {
+            that.addClass("non-default-value");
+        }
+      }
+
+    });
+  }
+  
+  function initialize_page() {
+    $('#reset-button').click();
+  }
+  
+  // 'program emtry point'
+  create_css_settings_controls();
+  initialize_page();
 
 })(jQuery);
