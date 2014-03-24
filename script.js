11bcc8ed980ce6b484d892f739b9ff28a1b912b1
Michael Lewandowski <milewgit@gmail.com>
2014-03-24 17:05:31 -0400

    Add additional comments to script.js

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+/*
+    The HTML page consists of two areas, a css settings area and an example
+    area.  At runtime, the settings area contains a number of css settings
+    for various HTML elements, for instance padding-left for li.  Each 
+    setting has a few labels, a readonly text box to show the setting value 
+    (e.g. 2em), and a range control used to vary the setting value.  The 
+    example area contains a layout made up of the elements with settings in 
+    the settings area, e.g. li.  As the slider for a given setting is varied,
+    the value in the associated text box is updated to reflect the slider
+    value and the css value in the example area is also updated.
+
+    At design time, a setting in the settings area is represented by a div 
+    placehold that has custom data- values to indicate the element that the 
+    setting applies to (li), the attribute that the setting applies to 
+    (padding-left), and optional initial, min and max slider values.  When 
+    the page loads, the code below populates the div placeholders with the 
+    labels, text boxes, and sliders described above, and adds a handler to 
+    the slider control that updates the text box and css value on the 
+    example element.
+
+    The settings area also has a Preset button and a Reset button.  Clicking
+    on the Preset button sets all of the css attributes to predefined values
+    that supposedly result in a pleasing layout in the example area.  Clicking
+    on the Reset button sets all of the css attributes to default values.
+
+    The custom data attributes data-preset-value and data-default-value can 
+    be used on the div placeholders to specify the preset and default css
+    attribute values, respectively.  The code below adds handlers to the two
+    buttons to populate the settings controls with preset and default values.
+ */
+
 (function($){
-  
+
+  // These functions run at page load time.
   populate_all_css_settings_placeholders();
   initialize_page();
   
@@ -17,8 +49,9 @@
   
     // The functions below are nested so that they can
     // references the placeholder, specs, and controls
-    // variables instead of passing them all over
-    // the place.
+    // variables instead of passing them all over the 
+    // place.  This means, however, that copies of such
+    // functions will be created for each placeholder.
     
     function install_event_handlers() {
       install_range_control_handler();
@@ -31,7 +64,9 @@
         set_setting_value(this.value);    // 'this' points to controls.range_control
       });
     }
-    
+
+    // Note that there will be one handler per placeholder, so 
+    // effectively the buttons will each have lists of observers.
     function install_preset_button_handler() {
       preset_button().on('click', function() {
         set_setting_value(specs.preset_value);
